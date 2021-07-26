2c151fca62399d7052e86dd9bdf6a1f6c93b0565
Rainer-Anton Englisch <rainer-anton.englisch@telekom.de>
2021-07-26 14:43:18 +0200

    update

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -40,6 +40,7 @@ getNextWords =function(textInput) {
   print("getNextWords")
   print(textInput)
   text_split = str_split(str_trim(textInput), pattern=" ", simplify = TRUE)
+  print(paste0("length of text_split ", toString(length(text_split))))
   print(text_split )
   result=NA
   
@@ -48,17 +49,23 @@ getNextWords =function(textInput) {
     preceding_gram = as.list(text_split[(length(text_split)-2):length(text_split)])
     print(preceding_gram)
     result = n_grams[[4]][preceding_gram,c("gram_4","p_kn"), on = c("gram_1","gram_2","gram_3")]
+    
   }      
-  if (length(text_split)==2  | is.na(result$p_kn)) {
+  if (length(text_split)==2  | is.na(result)) {#| is.na(result$p_kn)) {
+    print("Using split ==2")
     result = n_grams[[3]][as.list(text_split[(length(text_split)-1):length(text_split)]),c("gram_3","p_kn"), on = c("gram_1","gram_2")]
   }
-  if (length(text_split)==1 | is.na(result$p_kn)) {
+  if (length(text_split)==1 | is.na(result)) {#| is.na(result$p_kn)) {
+    print("Using split ==1")
     result = n_grams[[2]][as.list(text_split[length(text_split)]),c("gram_2","p_kn"), on = c("gram_1")]
     # for no word entered, need BOS 2-gram
     # if no word found use unigram?
   }
   result=result[order(result$p_kn, decreasing = TRUE),]  
-
+  #setnames(result, 1, gram)
+  colnames(result)[1] = "gram"
+  #result$gram <- as.character(result$gram)
+  result[,gram := as.character(gram)]
   return (result)
   #print(lapply(result[1:3,1:2], as.character))
 }
@@ -68,25 +75,29 @@ server <- function(input, output, session) {
   #cur_next_words = c("","","")
   cur_next_words = reactive({
       my_cur_next_words = getNextWords( input$textInput)
-      print(my_cur_next_words[1])
-      my_cur_next_words[,1:=as.character(1)]
+      print("Next word predictions:")
       print(my_cur_next_words[1:3])
+      
+      updateActionButton(session, inputId = "nextWord1", label = toString(my_cur_next_words[1,1]))
+      updateActionButton(session, inputId = "nextWord2", label = toString(my_cur_next_words[2,1]))
+      updateActionButton(session, inputId = "nextWord3", label = toString(my_cur_next_words[3,1]))
       my_cur_next_words
     })
   output$textNextWord1 = renderText({as.character(cur_next_words()[1,1])})
-  output$textNextWord2 = renderText({toString(cur_next_words()[2,1])})
-  output$textNextWord3 = renderText({toString(cur_next_words()[3,1])})
+  output$textNextWord2 = renderText({as.character(cur_next_words()[2,1])})
+  output$textNextWord3 = renderText({as.character(cur_next_words()[3,1])})
   
-  updateActionButton(session, inputId = "nextWord1", label = cur_next_words()[1,1])
-  updateActionButton(session, inputId = "nextWord2", label = cur_next_words()[2,1])
-  updateActionButton(session, inputId = "nextWord3", label = cur_next_words()[3,1])
   
   observeEvent(input$nextWord1, {
-    updateTextInput(session,inputId = "textInput", value = )
-    session$sendCustomMessage(type = 'testmessage',
-                              message = 'Thank you for clicking')
+    updateTextInput(session,inputId = "textInput", value = paste0(input$textInput," ",toString(cur_next_words()[1,1])))
   })
-
+  observeEvent(input$nextWord2, {
+    updateTextInput(session,inputId = "textInput", value = paste0(input$textInput," ",toString(cur_next_words()[2,1])))
+  })
+  observeEvent(input$nextWord3, {
+    updateTextInput(session,inputId = "textInput", value = paste0(input$textInput," ",toString(cur_next_words()[3,1])))
+  })
+  
   output$textIndex = renderText( {
     text_split = str_split(str_trim(input$textInput), pattern=" ", simplify = TRUE)
     toString(text_split[(length(text_split)-2):length(text_split)])})
